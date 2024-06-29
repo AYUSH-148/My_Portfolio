e55d4c1fe88b4266698937f699e6a673ee8e95ba
Ayush <ayushpro111@gmail.com>
2024-06-29 21:52:14 +0530

    exp fix

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                    I am new to the professional field but always eager to demonstrate my skills. Below are my experiences.
                 </Desc>
                 <TimelineSection>
                     <Timeline>
