f67898f7fcb724e8738426a1dacc44517960788d
Ayush <ayushpro111@gmail.com>
2024-06-29 13:31:49 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
+        </Desc>
+       
+        <CardContainer>
+          {projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,344 @@
+export const Bio = {
+  name: "Ayush Negi",
+  roles: [
+    "Full Stack Developer",
+    "Nextjs Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/AYUSH-148/",
+  resume:
+    "https://drive.google.com/file/d/1bs30j5nLROaTPKmcCaxOX80r9Z-9_Vte/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
+  insta: "https://www.instagram.com/aayush_007/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+     
+      {
+        name: "TypeScript",
+        image:
+          "https://imgs.search.brave.com/7mZ5KbtgXwUmyMVpggFAtFqWTh_aIk4F-p3edyjZwZE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi80LzRjL1R5/cGVzY3JpcHRfbG9n/b18yMDIwLnN2Zy8y/MjBweC1UeXBlc2Ny/aXB0X2xvZ29fMjAy/MC5zdmcucG5n",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://imgs.search.brave.com/nVyDpYrgvlKZUOIsHihVIipKzvjG4Rg7EXHWPPGy284/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvODJmYTczY2Fi/MDNjOWMxNTVmZTRl/ODgzMDMyYTU0MGI2/MGYxZTFkNzhkMWZl/YmQ5OWNkN2E3Mzcx/NWUzY2VkYi90YWls/d2luZGNzcy5jb20v",
+      },
+     
+      {
+        name: "Shadcn/ui",
+        image:
+          "https://seeklogo.com/images/S/shadcn-ui-logo-EF735EC0E5-seeklogo.com.png?v=638421451470000000",
+      },
+      {
+        name: "Flowbite",
+        image:
+          "https://imgs.search.brave.com/NuQN8qrBZXAH8JMHrDYMKuFIek5LSrcx2dPjB2x-QuM/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMTIxMjY1YWRi/MTRkOWFiMjFmODk4/YjM1OTlmMTgzNDE2/NzdlNDY0ZGQyMGRl/YTVlMTJhZmM2ZDk4/ODhmYzdjNi9mbG93/Yml0ZS5jb20v",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://imgs.search.brave.com/sFAukgJD-nbgqVp-6HA0hWnCXZ9QoE2guQ5ygS-ZowU/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvM2Y4YmU0NGQ3/ODQyMGE1YzhiZjZi/NTI1MzAzNDkzY2Rk/NmI2YzcyNGU1M2Fl/NjZmZDhlMWIyY2Y1/M2Q2ODExMS9leHBy/ZXNzanMuY29tLw",
+      },
+      {
+        name: "Redis",
+        image: "https://imgs.search.brave.com/0XydAktjaqN0nORmaggM2VJsY1Tx1nJiVNuih_WpnBU/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvYzAzNjQzNTY5/NTYzYTQyYzdmZmRl/ZTNiMzMwYWZhZTgw/MzQwN2JkMDg5NDcy/N2E3NzBkNDUxODIx/MWM3YWU5Mi9yZWRp/cy5pby8",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "NextAuth",
+        image:
+          "https://imgs.search.brave.com/pBRtWwLyULAwZXQjHYcr5gfV6IV9yxW4ThVILv-0FA4/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvM2YyNmQ1NDI3/YmUxMDA3NDQyN2Mz/MDMyNTQ2ODU5OWFi/Y2RhNzc5YzkxZmI2/ZGE2YmUxZTczYzVl/NWU4MWNmNC9uZXh0/LWF1dGguanMub3Jn/Lw",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://imgs.search.brave.com/BNSQrXn2bmeJXUV454rdF3_0T8nAzszmcKdxq9LZbwQ/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvZDQ5YzljMTg4/MmJjMjJkYzEzMWZi/YmYyODM3ZjEyYzEw/MjY5OWFlM2I5YTY1/YmZhNTJlY2EwM2Iz/N2RhMWIxOC92ZXJj/ZWwuY29tLw",
+      },
+      {
+        name: "Render",
+        image:
+          "https://imgs.search.brave.com/iUIiF_cAk0r_1kJIy86Pm60sPFTqbGRgFliHYBjXTUA/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyYjU4ODU5/NGVkMzFhYTA3Mzg3/N2UwZjZjNzQ3ZTc0/MjE2NmU3YjhlZWNi/MGFjNWRjYTJmNjA5/NjdhMzQ3Mi9yZW5k/ZXIuY29tLw",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://imgs.search.brave.com/6aVW64lywy3pCZ92XZZWT4x2dCJZAfThpPqiODIo7gI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jb21t/dW5pdHkuY2hvY29s/YXRleS5vcmcvY29u/dGVudC9wYWNrYWdl/aW1hZ2VzL3Bvc3Rt/YW4uMTEuMS4wLnBu/Zw",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://imgs.search.brave.com/1eVxRXPc_Z0OfT7PKWUnXwyQVS9SGaesrxbw3_f9p4g/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8zLzMzL0Zp/Z21hLWxvZ28uc3Zn/LzIyMHB4LUZpZ21h/LWxvZ28uc3ZnLnBu/Zw",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://imgs.search.brave.com/7cVOFDBkBYBy6vzfQl9pYittIldCaM3BYvXZWTTwF5o/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvZjg4ZDcxYmQ2/ZjkzMGEyNjUyNGJh/MjY0YjI4YmJmMWQz/YTcwNWIwOTFjNzUx/YmNlMWNmNmFkYTJl/NDA3YTdmMy93d3cu/Y2FudmEuY29tLw",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://imgs.search.brave.com/YRbW9et1as2nJdk2Q-CwUXyJ3e19eGd8Fr5WVRldCiI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Yi9iMy9BdGFsX0Jp/aGFyaV9WYWpwYXll/ZV9JbmRpYW5fSW5z/dGl0dXRlX29mX0lu/Zm9ybWF0aW9uX1Rl/Y2hub2xvZ3lfYW5k/X01hbmFnZW1lbnQl/MkNfR3dhbGlvcl9s/b2dvLmpwZy81MTJw/eC1BdGFsX0JpaGFy/aV9WYWpwYXllZV9J/bmRpYW5fSW5zdGl0/dXRlX29mX0luZm9y/bWF0aW9uX1RlY2hu/b2xvZ3lfYW5kX01h/bmFnZW1lbnQlMkNf/R3dhbGlvcl9sb2dv/LmpwZw",
+    school: "Indian Institute of Information Technlogy and Management, Gwalior",
+    date: "Nov 2022 - July 2027",
+    grade: "7.55 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Abv - Indian Institute of Information Technlogy and Management, Gwalior, Bhubaneswar. I have completed 4 semesters and have a CGPA of 7.55. I have learned  Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. ",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://imgs.search.brave.com/TT6LKXUW8GjrtBcl3ZbJa3iuIT17YFpLd4YLs8Irr2I/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMzVkYWUxNWM4/Zjg4YTk5ZjI3YmU5/YzVmMzM3NGE1Y2Zj/NmVkYThjM2E2M2E2/ZDMwYmExNDhhNTY4/NTIxMTczNS9kZXdh/bnB1YmxpY3NjaG9v/bG1lZXJ1dC5vcmcv",
+    school: "Dewan Public School, Meerut",
+    date: "Apr 2021 - Apr 2022",
+    grade: "96%",
+    desc: "I completed my class 12 high school education at Dewan Public School, Meerut, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://imgs.search.brave.com/TT6LKXUW8GjrtBcl3ZbJa3iuIT17YFpLd4YLs8Irr2I/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMzVkYWUxNWM4/Zjg4YTk5ZjI3YmU5/YzVmMzM3NGE1Y2Zj/NmVkYThjM2E2M2E2/ZDMwYmExNDhhNTY4/NTIxMTczNS9kZXdh/bnB1YmxpY3NjaG9v/bG1lZXJ1dC5vcmcv",
+    school: "Dewan Public School, Meerut",
+    date: "Apr 2019 - Apr 2020",
+    grade: "94%",
+    desc: "I completed my class 10 education at Dewan Public School, Meerut, where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const projects = [
+   
+  {
+    id: 0,
+    title: "TalkNet",
+    date: "Dec 2023 - jan 2024",
+    description:
+      "The primary objective behind developing this website is to replicate the functionality of Discord, offering users a platform for real-time communication and collaboration. Used WebSockets to enable real-time communication. Used LiveKit.io for video and audio communications and Clerk app for authentication",
+    image:
+      "https://github.com/AYUSH-148/Assets-Images/blob/main/Screenshot%202024-06-29%20125432.png?raw=true",
+    tags: ["Next Js", "PostgreSql", "Prisma", "Shadcn/ui", "Socket.io"],
+    category: "web app",
+    github: "https://github.com/AYUSH-148/discord-clone",
+    webapp: "https://discord-clone-nu-pink.vercel.app/",
+    member: [
+      {
+        name: "Ayush Negi",
+        img: "https://avatars.githubusercontent.com/u/139256944?v=4",
+        linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
+        github: "https://github.com/AYUSH-148/discord-clone",
+      },
+     
+    ],
+  },
+  
+ 
+
+  {
+    id: 1,
+    title: "Breaddit",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "It is a social news aggregation and discussion website. Users can submit content, such as text posts, links, and images, which are then voted up or down by other users. Content is organized into user-created communities called ”subreddits,” each focused on a specific topic or interest.",
+    image:
+      "https://github.com/AYUSH-148/Assets-Images/blob/main/Screenshot%202024-06-03%20220420.png?raw=true",
+    tags: ["Next Js", "MySql", "Prisma", "Shadcn/ui", "React-Query" ,"EditorJs"],
+    category: "web app",
+    github: "https://github.com/AYUSH-148/Breadit",
+    webapp: "https://breadit-1.onrender.com/",
+    member: [
+      {
+        name: "Ayush Negi",
+        img: "https://avatars.githubusercontent.com/u/139256944?v=4",
+        linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
+        github: "https://github.com/AYUSH-148/Breadit",
+      }, 
+    ],
+  },
+  {
+    id: 8,
+    title: "Ecommerce",
+    date: "Jan 2021",
+    description:
+      "It is a shopping website with an Admin dashboard. It is my first full-fledged project which I made while learning React. This project is not very UI friendly but it helped me learn a lot.",
+    image:
+      "https://github.com/AYUSH-148/Assets-Images/blob/main/Screenshot%202024-06-29%20132507.png?raw=true",
+    tags: ["React Js","Redux" ,"Node","Express","MongoDB","Chartjs", "Stripe"],
+    category: "web app",
+    github: "https://github.com/AYUSH-148/Ecommerce",
+    webapp: "https://ecommerce-lovat-two.vercel.app/",
+    member: [
+      {
+        name: "Ayush Negi",
+        img: "https://avatars.githubusercontent.com/u/139256944?v=4",
+        linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
+        github: "https://github.com/AYUSH-148/discord-clone",
+      }, 
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
