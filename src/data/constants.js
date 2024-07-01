51700e863fba6a9592ad824f8071d9b7d558c7ee
Ayyush <139256944+AYUSH-148@users.noreply.github.com>
2024-07-01 11:54:54 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,9 +1,10 @@
 export const Bio = {
   name: "Ayush Negi",
   roles: [
+    "Programmer",
     "Full Stack Developer",
     "Nextjs Developer",
-    "Programmer",
+   
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
