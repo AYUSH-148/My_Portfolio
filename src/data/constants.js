caa3f56e8a9be6c5e6656c69d7509305087cf25c
Ayush Negi <ayushpro111@gmail.com>
2025-07-14 17:15:43 +0530

    Update Resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AYUSH-148/",
   resume:
-    "https://drive.google.com/file/d/13QOIi2Ssy7XllvI9Ww9ygJ1iPxhhDAuK/view?usp=sharing",
+    "https://drive.google.com/file/d/1IMc7jwUCqHOvM0HXCF6-C3QnN9vzbp_m/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
   insta: "https://www.instagram.com/aayush_007/",
 };
