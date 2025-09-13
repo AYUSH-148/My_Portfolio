6ef5d0f1c236b3fd76d83e06109b6acfe42c3458
Ayush Negi <ayushpro111@gmail.com>
2025-09-14 03:57:22 +0530

    Change resume link in constants.js
    Updated resume link to a new Google Drive URL.

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AYUSH-148/",
   resume:
-    "https://drive.google.com/file/d/14hylggaKkUirusKGfJOMNK0tk5s0NR2d/view?usp=sharing",
+    "https://drive.google.com/file/d/18sDPyXzTYZtnjt1nKBizZQzgFljeyA-9/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ayush-negi-300209257/",
   insta: "https://www.instagram.com/aayush_007/",
 };
