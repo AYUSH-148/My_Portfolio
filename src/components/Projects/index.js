631e997db7d446beea1bc10d1a7d09d266fcaaa2
AYUSH-148 <ayushpro111@gmail.com>
2026-06-14 00:20:00 +0530

    Fix Vercel build: remove unused imports and invalid anchor markup

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import { Container, Wrapper, Title, Desc, CardContainer } from './ProjectsStyle'
 import ProjectCard from '../Cards/ProjectCards'
 import { projects } from '../../data/constants'
 
 
 const Projects = ({openModal,setOpenModal}) => {
-  const [toggle, setToggle] = useState('all');
+  
   return (
     <Container id="projects">
       <Wrapper>
